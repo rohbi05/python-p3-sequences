008d1e2b7ab9f2ce50ce817d1a21ee6a6d31a26e
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-08-29 15:50:26 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    
+    fibonacci = [0, 1]
+    
+    for _ in range(2, length):
+        next_value = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_value)
+    
+    print(fibonacci)
